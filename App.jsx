dd6ccbe20484c0e7a0a48a9d93679b571b1f9858
williantsi <williantsi@gmail.com>
2026-04-16 21:07:07 -0300

    Add WebView to load external URL in App component

--- App.jsx
+++ App.jsx
@@ -1,1 +1,10 @@
+import { WebView } from 'react-native-webview';
 
+export default function App() {
+  return (
+    <WebView 
+      source={{ uri: 'https://cajui.ifnmg.edu.br' }} 
+      style={{ flex: 1 }} 
+    />
+  );
+}
